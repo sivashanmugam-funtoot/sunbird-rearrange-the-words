1ead1d522e793edbd5611e4c14e7b3394e00f6ac
sivashanmugam-funtoot <sivashanmugam.kannan@funtoot.com>
2018-08-13 16:24:22 +0530

    updating all

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+var SEQController = SEQController || {};
+
+SEQController.onDomReady = function () {
+    SEQController.isQuestionTextOverflow();
+    $(document).ready(function () {
+        $(".option-container").sortable();
+        $(".option-container").disableSelection();
+    })
+}
+
+SEQController.isQuestionTextOverflow = function () {
+    if ($('.hiding-container').height() > $('.expand-container').height()) {
+        $('.expand-button').css('display', 'none');
+    } else {
+        $('.expand-button').css('display', 'block');
+    }
+}
+
+SEQController.toggleQuestionText = function(){
+    if($('.hiding-container').hasClass('expanded')){
+        $('.hiding-container').css('height', '50%');
+        $('.hiding-container').removeClass('expanded')
+        $(".expand-button img").toggleClass('flip');
+        $('.hiding-container').css('padding-bottom', '0px');
+        $('.expand-button').css('bottom', '5%');
+    } else {
+        var expandButtonBottom = parseFloat($('.expand-button').css('bottom'));
+        $('.hiding-container').addClass('expanded')
+        $('.hiding-container').css('height', 'auto');
+        $(".expand-button img").toggleClass('flip');
+        $('.hiding-container').css('padding-bottom', $(".expand-button").height()+'px');
+        expandButtonBottom = expandButtonBottom - ($('.hiding-container').height() - $('.question-container').height());
+        $('.expand-button').css('bottom', expandButtonBottom+'px')
+    }
+}
+
+SEQController.onDomReady()
+
